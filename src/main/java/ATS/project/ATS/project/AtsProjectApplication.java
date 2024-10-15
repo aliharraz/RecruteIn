75d4bd4c66d16484cbf56d72f7777b2fdf796182
VhagarGT <b.elafoui@metave.us>
2024-10-15 17:13:18 +0100

    Project structure

---
+++ src/main/java/ATS/project/ATS/project/AtsProjectApplication.java
@@ -0,0 +1,13 @@
+package ATS.project.ATS.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsProjectApplication.class, args);
+	}
+
+}
